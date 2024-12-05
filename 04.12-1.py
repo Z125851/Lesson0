8735b2bd502e63f6da0c03977d6c676acc221000
Z125851 <Z125851@outlook.com>
2024-12-05 21:05:31 +0300

    Add files via upload

---
+++ 04.12-1.py
@@ -0,0 +1,12 @@
+#"1st program"
+print((9**0,5)*5)
+#"2st program"
+print(9.99>9,98 and 1000 != 1000,1)
+#"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print (2*2+2) == (2*(2+2))
+#"4st program"
+print(123.456)
+print(int(123.456))
+print()
